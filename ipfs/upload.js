d3b78782e5333f26d56901d77404873cf8a8fc8c
Dan Nolan <dan.nolan@alchemy.com>
2023-01-06 12:30:42 -0500

    update image cid

--- ipfs/upload.js
+++ ipfs/upload.js
@@ -22,7 +22,7 @@ async function run() {
             }
             ],
             // update the IPFS CID to be your image CID
-            image: "ipfs://QmQ2wnwaFJ1w42UTywTWpM8RgiqrWwKFR6AMrpyiHPgi3p/",
+            image: "https://ipfs.io/ipfs/QmQ2wnwaFJ1w42UTywTWpM8RgiqrWwKFR6AMrpyiHPgi3p",
             description: "So much PLW3!"
         })
     };
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -11,7 +11,7 @@ async function main() {
   // mint one to yourself!
   const signer0 = await ethers.provider.getSigner(0);
   // update the IPFS CID to be your metadata CID
-  await nft.safeMint(await signer0.getAddress(), "ipfs://QmTDzeiBqFoMwxocRBhphRchxbsaacjuCw4wRfcJ2kJpvJ");
+  await nft.safeMint(await signer0.getAddress(), "ipfs://QmbCwoJB7g9b86BfEahsjzyzzNx3WEu7Jc1opXWgeTXFgb");
 
   console.log("NFT Minted!");
 }
