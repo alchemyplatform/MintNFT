1c994c3271a69b9dcf16ed94b401c98322941723
Dan Nolan <dan.nolan@alchemy.com>
2023-01-06 12:16:01 -0500

    init

---
+++ ipfs/upload.js
@@ -0,0 +1,36 @@
+async function run() {
+    const { create } = await import('ipfs-http-client');
+    const ipfs = await create();
+    
+    // we added three attributes, add as many as you want!
+    const metadata = {
+        path: '/',
+        content: JSON.stringify({
+            name: "My First NFT",
+            attributes: [
+            {
+                "trait_type": "Peace",
+                "value": "10" 
+            },
+            {
+                "trait_type": "Love",
+                "value": "100"
+            },
+            {
+                "trait_type": "Web3",
+                "value": "1000"
+            }
+            ],
+            // update the IPFS CID to be your image CID
+            image: "ipfs://QmQ2wnwaFJ1w42UTywTWpM8RgiqrWwKFR6AMrpyiHPgi3p/",
+            description: "So much PLW3!"
+        })
+    };
+
+    const result = await ipfs.add(metadata);
+    console.log(result);
+
+    process.exit(0);
+}
+
+run();
